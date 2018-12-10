abf8efebd8044b5a273fd6c0af06b8f330f31cbb
mikemutuma <mikemutuma664@gmail>
2018-12-10 15:12:36 +0300

    first commit

---
+++ app/src/main/java/com/mike/tabapp/StudentsFragment.java
@@ -0,0 +1,35 @@
+package com.mike.tabapp;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class StudentsFragment extends Fragment {
+    Button gusa;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View root = inflater.inflate(R.layout.fragment_students, container, false);
+        gusa = root.findViewById(R.id.btnsdt);
+        gusa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Umenigusa", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return root;
+    }
+
+}
